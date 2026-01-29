67f8022839d395a348399010425ff160934bd35a
pmguzumbi <143409778+pmguzumbi@users.noreply.github.com>
2026-01-29 10:13:16 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,39 @@
 # Input variables
+
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Project name used for resource naming"
+  type        = string
+  default     = "simple-log-service"
+}
+
+variable "environment" {
+  description = "Environment name (dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "log_retention_days" {
+  description = "CloudWatch log retention in days"
+  type        = number
+  default     = 30
+}
+
+variable "enable_deletion_protection" {
+  description = "Enable deletion protection for DynamoDB table"
+  type        = bool
+  default     = true
+}
+
+variable "compliance_email" {
+  description = "Email address for compliance notifications"
+  type        = string
+  default     = ""
+}
+
+
